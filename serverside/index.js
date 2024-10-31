64d2d3e3ee3b22c4a0b03d4f6faf23677809c4e2
jishnu2021 <jishnughosh98@gmail.com>
2024-10-31 19:05:54 +0530

    update cors

--- serverside/index.js
+++ serverside/index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGODB_URL;
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: 'http://35.154.178.145:5173',
+    credentials: true,
+  }));
 
 mongoose.connect(MONGO_URL, {
         useNewUrlParser: true,
